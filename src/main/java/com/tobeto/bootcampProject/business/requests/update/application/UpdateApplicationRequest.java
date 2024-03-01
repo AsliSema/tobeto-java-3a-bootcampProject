d55c03d6d37ac96bc12993ba66acd516d282cc4c
Asli Sema Gultekin <aslisemagultekin@gmail.com>
2024-03-01 23:35:56 +0300

    add validation annotations to UpdateApplicationRequest

--- src/main/java/com/tobeto/bootcampProject/business/requests/update/application/UpdateApplicationRequest.java
+++ src/main/java/com/tobeto/bootcampProject/business/requests/update/application/UpdateApplicationRequest.java
@@ -1,5 +1,6 @@
 package com.tobeto.bootcampProject.business.requests.update.application;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,8 +9,16 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class UpdateApplicationRequest {
+
+    @NotNull
     private int id;
+
+    @NotNull(message = "Applicant Id must not be null!")
     private int applicantId;
+
+    @NotNull(message = "Bootcamp Id must not be null!")
     private int bootcampId;
+
+    @NotNull(message = "Bootcamp State Id must not be null!")
     private int applicationStateId;
 }
