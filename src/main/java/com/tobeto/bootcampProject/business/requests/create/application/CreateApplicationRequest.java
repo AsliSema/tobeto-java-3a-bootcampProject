e42d68db91bd287d9ebfe1550a90d6f0e9f79242
Asli Sema Gultekin <aslisemagultekin@gmail.com>
2024-03-01 22:42:11 +0300

    add validation annotations to CreateApplicationRequest

--- src/main/java/com/tobeto/bootcampProject/business/requests/create/application/CreateApplicationRequest.java
+++ src/main/java/com/tobeto/bootcampProject/business/requests/create/application/CreateApplicationRequest.java
@@ -3,6 +3,7 @@ package com.tobeto.bootcampProject.business.requests.create.application;
 import com.tobeto.bootcampProject.entities.Applicant;
 import com.tobeto.bootcampProject.entities.ApplicationState;
 import com.tobeto.bootcampProject.entities.Bootcamp;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,7 +12,13 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class CreateApplicationRequest {
+
+    @NotNull(message = "Applicant Id must not be null!")
     private int applicantId;
+
+    @NotNull(message = "Bootcamp Id must not be null!")
     private int bootcampId;
+
+    @NotNull(message = "Bootcamp State Id must not be null!")
     private int applicationStateId;
 }
