b346333d5dc939bedf4b26b5198e162797c27660
Asli Sema Gultekin <aslisemagultekin@gmail.com>
2024-03-01 22:34:46 +0300

    add validation annotations to CreateApplicationStateRequest

--- src/main/java/com/tobeto/bootcampProject/business/requests/create/applicationState/CreateApplicationStateRequest.java
+++ src/main/java/com/tobeto/bootcampProject/business/requests/create/applicationState/CreateApplicationStateRequest.java
@@ -1,5 +1,7 @@
 package com.tobeto.bootcampProject.business.requests.create.applicationState;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,5 +10,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class CreateApplicationStateRequest {
+
+    @NotEmpty(message = "Name must not be empty!")
     private String name;
 }
