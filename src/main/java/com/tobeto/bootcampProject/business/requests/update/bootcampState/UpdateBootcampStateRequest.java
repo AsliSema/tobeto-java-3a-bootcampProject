5f88b7ecc051f5d77272f90c7491ff85603ec713
Asli Sema Gultekin <aslisemagultekin@gmail.com>
2024-03-01 23:47:36 +0300

    add validation annotations to UpdateEmployeeRequest

--- src/main/java/com/tobeto/bootcampProject/business/requests/update/bootcampState/UpdateBootcampStateRequest.java
+++ src/main/java/com/tobeto/bootcampProject/business/requests/update/bootcampState/UpdateBootcampStateRequest.java
@@ -1,5 +1,7 @@
 package com.tobeto.bootcampProject.business.requests.update.bootcampState;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,6 +10,10 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class UpdateBootcampStateRequest {
+
+    @NotNull
     private int id;
+
+    @NotEmpty(message = "Name must not be empty!")
     private String name;
 }
