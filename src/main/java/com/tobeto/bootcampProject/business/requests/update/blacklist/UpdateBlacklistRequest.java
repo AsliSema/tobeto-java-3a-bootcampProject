db2b1148cb82d052ca5c179c5627c7b6497ead66
Asli Sema Gultekin <aslisemagultekin@gmail.com>
2024-03-01 23:38:14 +0300

    add validation annotations to UpdateBlacklistRequest

--- src/main/java/com/tobeto/bootcampProject/business/requests/update/blacklist/UpdateBlacklistRequest.java
+++ src/main/java/com/tobeto/bootcampProject/business/requests/update/blacklist/UpdateBlacklistRequest.java
@@ -1,6 +1,8 @@
 package com.tobeto.bootcampProject.business.requests.update.blacklist;
 
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,8 +13,15 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 public class UpdateBlacklistRequest {
+
+    @NotNull
     private int id;
+
+    @NotEmpty(message = "Reason must not be empty!")
     private String reason;
+
     private LocalDateTime date;
+
+    @NotNull(message = "Applicant ID must not be null!")
     private int applicantId;
 }
