ef547b6291ff5a1d17b3e49e9fb4850ed2f2e37e
Asli Sema Gultekin <aslisemagultekin@gmail.com>
2024-03-01 22:46:21 +0300

    add validation annotations to CreateBootcampRequest

--- src/main/java/com/tobeto/bootcampProject/business/requests/create/bootcamp/CreateBootcampRequest.java
+++ src/main/java/com/tobeto/bootcampProject/business/requests/create/bootcamp/CreateBootcampRequest.java
@@ -5,6 +5,8 @@ import com.tobeto.bootcampProject.entities.Instructor;
 import jakarta.persistence.Column;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,9 +18,19 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 public class CreateBootcampRequest {
+
+    @NotEmpty(message = "Name must not be empty!")
     private String name;
+
+    @NotNull(message = "Instructor Id must not be null!")
     private int instructorId;
+
+    @NotNull(message = "Start Date must not be null!")
     private LocalDateTime startDate;
+
+    @NotNull(message = "End Date must not be null!")
     private  LocalDateTime endDate;
+
+    @NotNull(message = "Bootcamp State Id must not be null!")
     private int bootcampStateId;
 }
