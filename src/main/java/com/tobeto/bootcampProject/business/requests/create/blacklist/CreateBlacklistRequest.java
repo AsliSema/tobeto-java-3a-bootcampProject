e0440cca968b98c6ea730c0ef22c35d640385cd3
Asli Sema Gultekin <aslisemagultekin@gmail.com>
2024-03-01 22:39:12 +0300

    add validation annotations to CreateBlacklistRequest

--- src/main/java/com/tobeto/bootcampProject/business/requests/create/blacklist/CreateBlacklistRequest.java
+++ src/main/java/com/tobeto/bootcampProject/business/requests/create/blacklist/CreateBlacklistRequest.java
@@ -1,10 +1,8 @@
 package com.tobeto.bootcampProject.business.requests.create.blacklist;
 
 
-import com.tobeto.bootcampProject.entities.Applicant;
-import jakarta.persistence.Column;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,7 +13,12 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 public class CreateBlacklistRequest {
+
+    @NotEmpty(message = "Reason must not be empty!")
     private String reason;
+
     private LocalDateTime date;
+
+    @NotNull(message = "Applicant ID must not be null!")
     private int applicantId;
 }
